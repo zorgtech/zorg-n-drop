899b88292baf26d323a28a4c6daa25921c2ac2b4
Hampton <herohamp@users.noreply.github.com>
2016-12-28 20:52:54 -0600

    Ok basic snapping
    I added basic snapping with a ease for finishing! I will finish it in the morning!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var snapRange = 60;
+var currentId = 3;
+
+
 function handle_mousedown(e) {
     window.my_dragging = {};
     my_dragging.pageX0 = e.pageX;
@@ -20,6 +24,18 @@ function handle_mousedown(e) {
             .off('mousemove', handle_dragging)
             .off('mouseup', handle_mouseup);
 
+        $(".snapable").each(function (index) {
+            if (this != my_dragging.elem && typeof this == "object") {
+                if (canSnap(my_dragging.elem, this)==true){
+                    var newCords=$(this).position();
+                    newCords.top+=$(this).height();
+                    $(my_dragging.elem).css({top: newCords.top, left: newCords.left});
+                }
+            }
+
+        });
+
+
 
     }
     $('body')
@@ -27,8 +43,30 @@ function handle_mousedown(e) {
         .on('mousemove', handle_dragging);
 }
 
-function initialize(element){
-    element.snapInfo={snapped:false};
+function canSnap(element1, element2) {
+    
+    var el1=$(element1).position();
+    var el2=$(element2).position();
+    
+    el2.top+=$(element2).height();
+    el2.left+=$(element2).width()/2;
+    el1.top+=$(element1).height();
+    el1.left+=$(element1).width()/2;
+    
+    var distance = Math.floor(Math.hypot(el2.left - el1.left, el2.top - el1.top));
+    if (distance <= snapRange) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function initialize(element) {
+    element.snapInfo = {
+        attachedTo: false, //Ok this is confusing but here is the difference! AttachedTo is the block it has snapped to and snappedTo is the block that snapped to it.
+        snappedTo:false,
+        
+    };
 }
 
 $('.draggables').mousedown(handle_mousedown);
